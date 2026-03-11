52702ca4557631c18ead2699a32d04ae811d131c
preethi-r04 <preethiliki2004@gmail.com>
2026-03-11 11:15:23 +0530

    [Preethi]: Added: UC3: Hello REST API using Path Variable

--- src/main/java/com/backend/controller/HelloController.java
+++ src/main/java/com/backend/controller/HelloController.java
@@ -2,18 +2,27 @@ package com.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // UC1 - GET Hello
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
 
+    // UC2 - Query Parameter
     @GetMapping("/hello/query")
     public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // UC3 - Path Variable
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }

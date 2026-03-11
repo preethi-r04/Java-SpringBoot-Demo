b82147d932da6e5254986143be4250714b000d1b
preethi-r04 <preethiliki2004@gmail.com>
2026-03-11 11:12:20 +0530

    [Preethi]: Added: UC2: Hello REST API using Query Parameter

--- src/main/java/com/backend/controller/HelloController.java
+++ src/main/java/com/backend/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
